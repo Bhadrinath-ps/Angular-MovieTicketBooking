f5c43a557ae67c9f3b29936f4f9289b4fea25b85
Bhadrinath-ps <103251938+Bhadrinath-ps@users.noreply.github.com>
2023-12-15 11:21:45 +0530

    Add files via upload

---
+++ dist/movie-booking/assets/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  registerUrl: 'http://localhost:3000/registration/',
+  totalDataUrl: 'http://localhost:3000/totaldata/',
+  contactUsUrl: 'http://localhost:3000/contact_us/',
+  movieNowShowingUrl: 'http://localhost:3000/movieNowShowing/',
+  movieUpcomingUrl: 'http://localhost:3000/movieUpcoming/',
+  movieSeatsUrl: 'http://localhost:3000/movieseats/',
+  foodBeveragesUrl: 'http://localhost:3000/foodBeverages/',
+};
